c0fa4585b5551a55734e94df5d099fe7ba1bd0a3
Šimon Bučko <sibu@queue-it.com>
2023-10-08 19:44:32 +0200

    connected to the db

--- server/app.js
+++ server/app.js
@@ -1,12 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
 
 dotenv.config();
 
+const sequalize = new Sequelize(
+  process.env.DB_SCHEMA_NAME,
+  process.env.DB_USER_NAME,
+  process.env.DB_USER_PASSWORD,
+  {
+    host: process.env.DB_CONNECTION_STRING,
+    dialect: "mysql",
+    logging: console.log,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+      },
+    },
+  }
+);
 const SERVER_PORT = process.env.SERVER_PORT || 8080;
 
 const app = express();
 
-app.listen(SERVER_PORT, () => {
-  console.log(`Server is running on port: ${SERVER_PORT}`);
+app.listen(SERVER_PORT, async () => {
+  try {
+    await sequalize.authenticate();
+    console.log("Successfully connected to DB");
+    console.log(`Server is running on port: ${SERVER_PORT}`);
+  } catch (error) {
+    console.log(error);
+  }
 });
